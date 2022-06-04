6ee3d0f1e7cdf83f281468bfdd79cd3755e37612
Jonas Ladner <jonas.ladner@tum.de>
2022-06-04 14:05:33 +0200

    add temporary mappings to make it easier to review

--- src/main/kotlin/de/reservationbear/eist/HelloControler.kt
+++ src/main/kotlin/de/reservationbear/eist/HelloControler.kt
@@ -5,6 +5,9 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.sql.Date
+import java.sql.Timestamp
+import java.util.*
 
 
 @RestController
@@ -21,6 +24,43 @@ class HelloController(val service: RestaurantService) {
         val sb: StringBuilder = StringBuilder()
         for (restaurant in res.content)
             sb.append(restaurant.id.toString() + " " + restaurant.name + " " + restaurant.website)
+
+        return sb.toString()
+    }
+
+    @GetMapping("DB-TEST-2")
+    fun db2(): String {
+        val paging: Pageable = PageRequest.of(0, 15)
+        val res = service.getPageOfRestaurantComments(
+            UUID.fromString("069f72db-2157-43de-8e88-21661b518200"), paging
+        );
+        val sb: StringBuilder = StringBuilder()
+        if (res != null) {
+            for (comment in res.content)
+                if (comment != null) {
+                    sb.append(comment.comment + " ")
+                }
+        }
+
+        return sb.toString()
+    }
+
+    @GetMapping("DB-TEST-3")
+    fun db3(): String {
+        val paging: Pageable = PageRequest.of(0, 15)
+        val res = service.findReservationsInTimeframeOfRestaurant(
+            UUID.fromString("069f72db-2157-43de-8e88-21661b518200"),
+            Timestamp(System.currentTimeMillis()),
+            Timestamp(System.currentTimeMillis() + 604800000),
+            paging
+        );
+        val sb: StringBuilder = StringBuilder()
+        if (res != null) {
+            for (reservation in res.content)
+                if (reservation != null) {
+                    sb.append(reservation.userName + " ")
+                }
+        }
 
         return sb.toString()
     }
