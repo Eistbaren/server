8b6be696d61a81b3b13e369b681b311c26834c3b
Jonas Ladner <jonas.ladner@tum.de>
2022-06-09 20:51:39 +0200

    remove unused empty ctor

--- src/main/kotlin/de/reservationbear/eist/HelloController.kt
+++ src/main/kotlin/de/reservationbear/eist/HelloController.kt
@@ -11,7 +11,7 @@ import java.util.*
 
 
 @RestController
-class HelloController() {
+class HelloController {
     @GetMapping("sayHello")
     fun sayHello(): String {
         return "Hello World!"
