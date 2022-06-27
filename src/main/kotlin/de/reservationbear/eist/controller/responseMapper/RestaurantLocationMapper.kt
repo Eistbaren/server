e48b0ac9a89ad186e125e596e45a1726250f2c0d
Jonas Ladner <jonas.ladner@tum.de>
2022-06-27 23:13:13 +0200

    add documentation

--- src/main/kotlin/de/reservationbear/eist/controller/responseMapper/RestaurantLocationMapper.kt
+++ src/main/kotlin/de/reservationbear/eist/controller/responseMapper/RestaurantLocationMapper.kt
@@ -2,6 +2,12 @@ package de.reservationbear.eist.controller.responseMapper
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * Mapper for reservation location
+ *
+ * @param confirmed
+ */
+
 class RestaurantLocationMapper(
     @field:JsonProperty("lat") val lat: Double,
     @field:JsonProperty("lon") val lon: Double
