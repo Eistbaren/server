86be3388963e7777f6f8374b198629f04cdce024
Simon Okutan <simon.ok@gmx.de>
2022-06-28 10:48:33 +0200

    Fix brackets in EistApplication.kt

--- src/main/kotlin/de/reservationbear/eist/EistApplication.kt
+++ src/main/kotlin/de/reservationbear/eist/EistApplication.kt
@@ -4,8 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EistApplication
+class EistApplication {
 
-fun main(args: Array<String>) {
-    runApplication<EistApplication>(*args)
+    fun main(args: Array<String>) {
+        runApplication<EistApplication>(*args)
+    }
 }
